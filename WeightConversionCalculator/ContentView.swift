aaaa1b78661cd609d0260a930d416097edd28043
Jared Molina <jared.a.molina01@gmail.com>
2022-04-20 10:44:36 -0700

    Initial Commit

---
+++ WeightConversionCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeightConversionCalculator
+//
+//  Created by jred on 4/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeightConversionCalculator/WeightConversionCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightConversionCalculatorApp.swift
+//  WeightConversionCalculator
+//
+//  Created by jred on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WeightConversionCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
